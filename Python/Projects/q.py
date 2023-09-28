c49581c107c8652a3434335ae011cbe6de700ef3
Mwadz <mghoimwadime@gmail.com>
2023-09-28 15:32:47 +0300

    py

---
+++ Python/Projects/q.py
@@ -0,0 +1,36 @@
+# a, b = 12, 5
+# if a + b:
+#     print("true")
+# else:
+#     print("false")
+
+# for num in range(10, 14):
+#     for i in range(2, num):
+#         if num%1 == 1:
+#             print(num)
+#         break
+# d = {"john":43, "peter":50}
+# print(d["john"])
+# print(len(d))
+# print(list(d.keys()))
+# def say(msg, times = 1):
+#     print(msg*times)
+# print(say("hello", 3))
+
+# def f(x=1, y=2):
+#     x = x+y
+#     y += 1
+#     print(x,y)
+# f(y=2,x=1)
+import numpy as np
+import pandas as pd
+data = pd.read_csv("")
+# n = np.array([[1,2,3,4,5,8],[1,2,3,4,5,6]])
+# print(n.size)
+# print(n.ndim)
+# a = np.zeros(6)
+# b = np.arange(0,10,2)
+# c = a+b
+# print(c[4])
+
+
